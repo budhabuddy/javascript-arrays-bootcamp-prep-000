4ff1e561ff63d86e6afa3ee6e15ecf0ff8047a55
joe rudd <bbpcash@gmail.com>
2018-09-18 23:36:07 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,5 +1,7 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"];
 
  
-var addElementToBeginingOfArray = ['array','element'];
+function addElementToBeginingOfArray(array,element);
 
+
+function destructivelyAddElementToBeginingOfArray();S
